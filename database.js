743a477a0a28ac93cd3928193b47c63be46c85b5
= <=>
2020-11-02 12:43:22 +0700

    init on pi

--- database.js
+++ database.js
@@ -1,56 +1,60 @@
 var sqlite3 = require('sqlite3').verbose()
 
-module.exports={
-    mySqlite_room_status(params){
-    let db = new sqlite3.Database('C:/sqlite/gui/test', (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log('Connected to the test database.');
-    });
+module.exports = {
+    mySqlite_room_status(params) {
+        // let db = new sqlite3.Database('E:/SV5_HK1/EdgeComputing_HealthCare/EdgeCompuitng_MQTT/database', (err) => {
+        let db = new sqlite3.Database('C:/sqlite/gui/test', (err) => {
+        // let db = new sqlite3.Database('./database/database', (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Connected to the test database.');
+        });
 
-    // var sql ='INSERT INTO Data (room, time, nhiet, am) VALUES (?,?,?,?)'
-    var sql ='INSERT INTO Data_room_status (room, time, nhiet, am) VALUES (?, ?, ?, ?)'
-    // var params =['room01', 27, 60.2]
-    params[1] = Date.now();
-            db.run(sql, params, function (err, result) {
-                if (err){
-                    console.error(err)
+        // var sql ='INSERT INTO Data (room, time, nhiet, am) VALUES (?,?,?,?)'
+        var sql ='INSERT INTO room_status (room, time, nhiet, am) VALUES (?, ?, ?, ?)'
+        // var params =['room01', 27, 60.2]
+        params[1] = Date.now();
+        db.run(sql, params, function (err, result) {
+            if (err){
+                console.error(err)
                     return;
-                }
-                else{
-                    console.log('ok')
-                }
-            });
+            }else{
+                console.log('ok')
+            }
+          });
     
-    db.close((err) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log('Close the database connection.');
-    });
-  },
+        db.close((err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Close the database connection.');
+        });
+    },
 
 
 
   mySqlite_device_status(params){
+    // let db = new sqlite3.Database('E:/SV5_HK1/EdgeComputing_HealthCare/EdgeCompuitng_MQTT/database', (err) => {
     let db = new sqlite3.Database('C:/sqlite/gui/test', (err) => {
+    //let db = new sqlite3.Database('ss./database/database', (err) => {
       if (err) {
         console.error(err.message);
       }
       console.log('Connected to the test database.');
     });
 
-    var sql ='INSERT INTO Room_device_status (room, device_id, name, status, time) values( ?, ?, ?, ?, ?)'
-    // var params =['room01', 27, 60.2]
-            params[4] = Date.now();
-            db.run(sql, params, function (err, result) {
+    // var sql ='INSERT INTO Room_device_status (room, device_id, name, status) values( ?, ?, ?, ?)'
+    params[1] = params[0] + params[2];
+    var sql ='UPDATE Room_device_status SET status = ? WHERE deviceid = ?';
+            // params.push(Date.now());
+            db.run(sql, [params[3], params[1]], function (err, result) {
                 if (err){
                     console.error(err)
                     return;
                 }
                 else{
-                    console.log('ok')
+                    console.log('update ' + params[1] + ' : ' + params[3]);
                 }
             });
 
@@ -62,10 +66,11 @@ module.exports={
     });
   },
 
+//////////////////////////////////////////////////////////////////////////////
 
-
-  async mySqliteRead_RoomStatus(){
+  mySqliteRead_RoomStatus(callback){
     let results = [];
+    // let db = new sqlite3.Database('E:/SV5_HK1/EdgeComputing_HealthCare/EdgeCompuitng_MQTT/database', (err) => {
     let db = new sqlite3.Database('C:/sqlite/gui/test', (err) => {
       if (err) {
         console.error(err.message);
@@ -73,9 +78,9 @@ module.exports={
       console.log('Connected to the test database.');
     });
 
-    var sql ='SELECT * FROM Data_room_status ORDER BY TIME DESC LIMIT 6'
+    var sql ='SELECT * FROM room_status ORDER BY TIME DESC LIMIT 6'
     // var params =['room01', 27, 60.2]
-            await db.each(sql,  function (err, result) {// chay dong thoi voi (1)
+            db.each(sql,  function (err, result) {// chay dong thoi voi (1)
                 if (err){
                     console.error(err)
                     return;
@@ -87,14 +92,14 @@ module.exports={
             });
             // console.log("======================================================================"); // this is (1)
             // console.log(results);
-    await db.close((err) => {
+    db.close((err) => {
       if (err) {
         console.error(err.message);
       }
       console.log('Close the database connection.');
       // console.log(results);
       // callback(results);
-      return results;
+      callback(err, results);
     });
     // console.log("chan may qua'");
     // console.log(results);
@@ -104,9 +109,12 @@ module.exports={
     // return results;
   },
 
+/////////////////////////////////////////////////////////////////////////////
 
-
-  mySqliteRead_DeviceStatus(params){
+  mySqliteRead_DeviceStatus(callback){
+    // let db = new sqlite3.Database('E:/SV5_HK1/EdgeComputing_HealthCare/EdgeCompuitng_MQTT/database', (err) => {
+    // let db = new sqlite3.Database('./database/database', (err) => {
+    let results = [];
     let db = new sqlite3.Database('C:/sqlite/gui/test', (err) => {
       if (err) {
         console.error(err.message);
@@ -122,7 +130,8 @@ module.exports={
                     return;
                 }
                 else{
-                    console.log(result)
+                    // console.log(result)
+                    results.push(result);
                 }
             });
     
@@ -131,7 +140,7 @@ module.exports={
         console.error(err.message);
       }
       console.log('Close the database connection.');
+      callback(err, results);
     });
-    
   }
 }
